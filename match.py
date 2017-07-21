1c4b529cf2c7ac738fab10540d078f0e5debd781
Tai Sakuma <tai.sakuma@gmail.com>
2017-07-21 10:35:49 +0100

    clean up

--- match.py
+++ match.py
@@ -100,14 +100,14 @@ class ObjectMatch(object):
 
     def event(self, event):
         self._attach_to_event(event)
-        in1 = getattr(event, self.obj1_name)
-        in2 = getattr(event, self.obj2_name)
+        obj1 = getattr(event, self.obj1_name)
+        obj2 = getattr(event, self.obj2_name)
 
-        self.obj1_matched[:], self.obj2_matched_sorted[:], self.obj1_unmatched[:], self.obj2_unmatched[:] = self._match(in1, in2)
+        self.obj1_matched[:], self.obj2_matched_sorted[:], self.obj1_unmatched[:], self.obj2_unmatched[:] = self._match(obj1, obj2)
 
-    def _match(self, in1, in2):
+    def _match(self, obj1, obj2):
 
-        distances = [[(i1, i2, self.distance_func(o1, o2)) for i1, o1 in enumerate(in1)] for i2, o2 in enumerate(in2)]
+        distances = [[(i1, i2, self.distance_func(o1, o2)) for i1, o1 in enumerate(obj1)] for i2, o2 in enumerate(obj2)]
         # a list of lists of (index1, index2, distance) grouped by index2
         # e.g., [[(0, 0, 0.12), (1, 0, 0.32)], [(0, 1, 0.10), (1, 0, 0.52)], ...]
 
@@ -130,11 +130,11 @@ class ObjectMatch(object):
         distances = [min(l, key = operator.itemgetter(2)) for l in distances]
         # select one with the minimum distance in each sublist
 
-        obj1_matched = [in1[i] for i, j, d in distances]
-        obj2_matched_sorted = [in2[j] for i, j, d in distances]
+        obj1_matched = [obj1[i] for i, j, d in distances]
+        obj2_matched_sorted = [obj2[j] for i, j, d in distances]
 
-        obj1_unmatched = [o for o in in1 if o not in obj1_matched]
-        obj2_unmatched = [o for o in in2 if o not in obj2_matched_sorted]
+        obj1_unmatched = [o for o in obj1 if o not in obj1_matched]
+        obj2_unmatched = [o for o in obj2 if o not in obj2_matched_sorted]
 
         return obj1_matched, obj2_matched_sorted, obj1_unmatched, obj2_unmatched
 
