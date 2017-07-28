ec2966a26debf719d9eb5390adf4bc9939c9b5a4
Tai Sakuma <tai.sakuma@gmail.com>
2017-07-28 15:13:19 +0100

    add a new scribbler JetAddMatchedObjects

--- delphes.py
+++ delphes.py
@@ -1,8 +1,15 @@
 # Tai Sakuma <tai.sakuma@cern.ch>
+import copy
+import itertools
+import operator
 import numpy as np
+
+import ROOT
 
 ## https://cp3.irmp.ucl.ac.be/projects/delphes/wiki/WorkBook/RootTreeDescription
 ## http://home.thep.lu.se/~torbjorn/pythia81html/ParticleProperties.html
+
+from .match import DeltaR
 
 ##__________________________________________________________________||
 class LheHT(object):
@@ -112,3 +119,112 @@ class MHT(object):
         self.out_phi[:] = [mht_phi]
 
 ##__________________________________________________________________||
+class JetAddMatchedObjects(object):
+    def __init__(self, in_obj1, in_obj2,
+                 out_obj1,
+                 distance_func = DeltaR(),
+                 max_distance = 0.4
+    ):
+        self.in_obj1_name = in_obj1
+        self.in_obj2_name = in_obj2
+        self.out_obj1_name = out_obj1
+        self.distance_func = distance_func
+        self.max_distance = max_distance
+
+    def __repr__(self):
+        name_value_pairs = (
+            ('in_obj1_name', self.in_obj1_name),
+            ('in_obj2_name', self.in_obj2_name),
+            ('out_obj1_name', self.out_obj1_name),
+            ('distance_func', self.distance_func),
+            ('max_distance', self.max_distance),
+        )
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join(['{} = {!r}'.format(n, v) for n, v in name_value_pairs]),
+        )
+
+    def begin(self, event):
+        self.out_obj1 = [ ]
+        self._attach_to_event(event)
+
+    def _attach_to_event(self, event):
+        setattr(event, self.out_obj1_name, self.out_obj1)
+
+    def event(self, event):
+        self._attach_to_event(event)
+
+        obj1 = getattr(event, self.in_obj1_name)
+        obj2 = getattr(event, self.in_obj2_name)
+
+        # each obj1 can be matched to multiple obj2s
+        # each obj2 can be matched to at most one obj1
+
+        distances = [[(i1, i2, self.distance_func(o1, o2)) for i1, o1 in enumerate(obj1)] for i2, o2 in enumerate(obj2)]
+        # a list of lists of (index1, index2, distance) grouped by index2
+        # e.g.,
+        # [
+        #     [(0, 0, 13.0), (1, 0, 10.0), (2, 0, 7.0), (3, 0, 4.0)],
+        #     [(0, 1, 6.5), (1, 1, 3.5), (2, 1, 0.5), (3, 1, 2.5)],
+        #     [(0, 2, 5.0), (1, 2, 2.0), (2, 2, 1.0), (3, 2, 4.0)],
+        #     [(0, 3, 2.0), (1, 3, 1.0), (2, 3, 4.0), (3, 3, 7.0)],
+        #     [(0, 4, 1.0), (1, 4, 2.0), (2, 4, 5.0), (3, 4, 8.0)]
+        # ]
+
+        distances = [l for l in distances if l]
+        # remove empty sublists
+
+        distances = (min(l, key = operator.itemgetter(2)) for l in distances)
+        # select one with the minimum distance in each sublist
+        # e.g., [(3, 0, 4.0), (2, 1, 0.5), (2, 2, 1.0), (1, 3, 1.0), (0, 4, 1.0)]
+
+        distances = (l for l in distances if l[2] <= self.max_distance)
+        # remove ones with distances greater than maximum distances
+        # e.g., [(2, 1, 0.5), (2, 2, 1.0), (1, 3, 1.0), (0, 4, 1.0)]
+        # note index1 == 2 happens twice
+
+        distances = sorted(distances, key = operator.itemgetter(0))
+        # sort by index1
+        # e.g., [(0, 4, 1.0), (1, 3, 1.0), (2, 1, 0.5), (2, 2, 1.0)]
+
+        distances = [list(g) for _, g in itertools.groupby(distances, key = operator.itemgetter(0))]
+        # group by index1
+        # e.g., [[(0, 4, 1.0)], [(1, 3, 1.0)], [(2, 1, 0.5), (2, 2, 1.0)]]
+
+        distances = {e[0][0]: [ee[1] for ee in e] for e in distances}
+        # convert to dict with the key index1
+
+
+        out = [ ]
+        for i, o1 in enumerate(obj1):
+
+            o1_copy = copy.copy(o1)
+
+            if not i in distances:
+                out.append(o1_copy)
+                continue
+
+            p4 = ROOT.TLorentzVector()
+            p4.SetPtEtaPhiM(o1.PT, o1.Eta, o1.Phi, o1.Mass)
+
+            o2s = [obj2[j] for j in distances[i]]
+
+            for o2 in o2s:
+
+                # this works only for GenParticle
+                p4_ = ROOT.TLorentzVector(o2.Px, o2.Py, o2.Pz, o2.E)
+                p4 += p4_
+
+            o1_copy.PT = p4.Pt()
+            o1_copy.Eta = p4.Eta()
+            o1_copy.Phi = p4.Phi()
+            o1_copy.Mass = p4.M()
+            out.append(o1_copy)
+
+        self.out_obj1[:] = out
+
+    def end(self):
+        self.out_obj1 = None
+
+##__________________________________________________________________||
+
