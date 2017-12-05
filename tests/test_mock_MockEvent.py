c7e8db944339a4d7a7979787083ba82499ff405a
Tai Sakuma <tai.sakuma@gmail.com>
2017-12-05 15:55:43 +0000

    reimplement test for MockEvent with pytest

--- tests/test_mock_MockEvent.py
+++ tests/test_mock_MockEvent.py
@@ -1,25 +1,25 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 
 from .mock import MockEvent
 
 ##__________________________________________________________________||
-class Test_MockEvent(unittest.TestCase):
+def test_setattr_same_object():
+    obj = MockEvent()
+    var1 = [ ]
+    obj.var1 = var1
+    obj.var1 = var1
 
-    def test_setattr_same_object(self):
-        obj = MockEvent()
-        var1 = [ ]
-        obj.var1 = var1
-        obj.var1 = var1
-
-    def test_setattr_different_objects(self):
-        obj = MockEvent()
+def test_setattr_different_objects():
+    obj = MockEvent()
+    obj.var1 = [ ]
+    with pytest.raises(ValueError):
         obj.var1 = [ ]
-        self.assertRaises(ValueError, obj.__setattr__, 'var1', [ ])
 
-    def test_getattr(self):
-        obj = MockEvent()
-        var1 = [ ]
-        obj.var1 = var1
-        self.assertIs(var1, obj.var1)
+def test_getattr():
+    obj = MockEvent()
+    var1 = [ ]
+    obj.var1 = var1
+    assert obj.var1 is var1
 
 ##__________________________________________________________________||
