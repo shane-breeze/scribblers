c9b30ad517564ab86e31d1a223f56427ce569161
Tai Sakuma <tai.sakuma@gmail.com>
2017-12-05 17:41:37 +0000

    reimplement test for SMSMass with pytest

--- tests/test_heppy_SMSMass.py
+++ tests/test_heppy_SMSMass.py
@@ -1,39 +1,40 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+
+import pytest
 
 from scribblers.heppy import SMSMass
 from .mock import MockEvent
 
 ##__________________________________________________________________||
-class Test_SMSMass(unittest.TestCase):
+@pytest.fixture()
+def obj():
+    return SMSMass()
 
-    def setUp(self):
-        self.obj = SMSMass()
+##__________________________________________________________________||
+def test_repr(obj):
+    repr(obj)
 
-    def test_repr(self):
-        repr(self.obj)
+def test_begin(obj):
+    event = MockEvent()
+    event.componentName = ['SMS_T1tttt']
+    event.GenSusyMGluino = [1800]
+    event.GenSusyMNeutralino = [200]
+    obj.begin(event)
+    assert event.smsmass1 is event.GenSusyMGluino
+    assert event.smsmass2 is event.GenSusyMNeutralino
 
-    def test_begin(self):
-        event = MockEvent()
-        event.componentName = ['SMS_T1tttt']
-        event.GenSusyMGluino = [1800]
-        event.GenSusyMNeutralino = [200]
-        self.obj.begin(event)
-        self.assertIs(event.GenSusyMGluino, event.smsmass1)
-        self.assertIs(event.GenSusyMNeutralino, event.smsmass2)
+def test_begin_massdict(obj):
+    massdict = {
+        'SMS-T2bb': ('GenSusyMSbottom', 'GenSusyMNeutralino'),
+    }
+    obj = SMSMass(massdict = massdict)
 
-    def test_begin_massdict(self):
-        massdict = {
-            'SMS-T2bb': ('GenSusyMSbottom', 'GenSusyMNeutralino'),
-        }
-        self.obj = SMSMass(massdict = massdict)
-
-
-        event = MockEvent()
-        event.componentName = ['SMS-T2bb_mSbottom-625to1050_0to550_25ns']
-        event.GenSusyMSbottom = [1000]
-        event.GenSusyMNeutralino = [200]
-        self.obj.begin(event)
-        self.assertIs(event.GenSusyMSbottom, event.smsmass1)
-        self.assertIs(event.GenSusyMNeutralino, event.smsmass2)
+    event = MockEvent()
+    event.componentName = ['SMS-T2bb_mSbottom-625to1050_0to550_25ns']
+    event.GenSusyMSbottom = [1000]
+    event.GenSusyMNeutralino = [200]
+    obj.begin(event)
+    assert event.smsmass1 is event.GenSusyMSbottom
+    assert event.smsmass2 is event.GenSusyMNeutralino
 
 ##__________________________________________________________________||
