ad2602e148fa966a040306ccb5e11edb0cd4cc9e
Tai Sakuma <tai.sakuma@gmail.com>
2017-12-05 18:05:51 +0000

    reimplement test for ScalePtByFactorOfPtEtaFromTbl with pytest

--- tests/test_jec_ScalePtByFactorsOfPtEtaFromTbl.py
+++ tests/test_jec_ScalePtByFactorsOfPtEtaFromTbl.py
@@ -1,4 +1,7 @@
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+
+import pytest
+
 import io
 
 from scribblers.jec import ScalePtByFactorOfPtEtaFromTbl
@@ -38,43 +41,40 @@ tbl_corr_txt = b"""
 """[1:] # to remove the fist line break
 
 ##__________________________________________________________________||
-class Test_ObjectCorrection(unittest.TestCase):
+@pytest.fixture()
+def obj():
+    f = io.BytesIO(tbl_corr_txt)
+    return ScalePtByFactorOfPtEtaFromTbl(
+        tbl_corr_path = f,
+        valid_eta_range = (-3, 3),
+        default_scale_factor = 0.8,
+    )
 
-    def setUp(self):
-        f = io.BytesIO(tbl_corr_txt)
-        self.obj = ScalePtByFactorOfPtEtaFromTbl(
-            tbl_corr_path = f,
-            valid_eta_range = (-3, 3),
-            default_scale_factor = 0.8,
-        )
+##__________________________________________________________________||
+def test_repr(obj):
+    repr(obj)
 
-    def tearDown(self):
-        pass
+def test_call(obj):
+    assert obj(pt = 37.9, eta = -0.8) == pytest.approx(44.83949)
 
-    def test_repr(self):
-        repr(self.obj)
+def test_scale_factor_simple(obj):
+    assert obj.scale_factor(pt = 37.9, eta = -0.8) == 1.1831
+    assert obj.scale_factor(pt = 37.9, eta = -0.4) == 1.1813
+    assert obj.scale_factor(pt = 38.1, eta = -0.8) == 1.1732
 
-    def test_call(self):
-        self.assertAlmostEqual(44.83949, self.obj(pt = 37.9, eta = -0.8))
+    # the last pt bin for each eta bin
+    assert obj.scale_factor(pt = 500.0, eta = -1.2) == 1.1186
+    assert obj.scale_factor(pt = 500.0, eta = 0.2) == 1.1118
+    assert obj.scale_factor(pt = 500.0, eta = 0.8) == 1.1186
 
-    def test_scale_factor_simple(self):
-        self.assertEqual(1.1831, self.obj.scale_factor(pt = 37.9, eta = -0.8))
-        self.assertEqual(1.1813, self.obj.scale_factor(pt = 37.9, eta = -0.4))
-        self.assertEqual(1.1732, self.obj.scale_factor(pt = 38.1, eta = -0.8))
+def test_scale_factor_out_of_range_pt(obj):
+    assert obj.scale_factor(pt = 20.1, eta = 0.2) == 1.1726
 
-        # the last pt bin for each eta bin
-        self.assertEqual(1.1186, self.obj.scale_factor(pt = 500.0, eta = -1.2))
-        self.assertEqual(1.1118, self.obj.scale_factor(pt = 500.0, eta = 0.2))
-        self.assertEqual(1.1186, self.obj.scale_factor(pt = 500.0, eta = 0.8))
+def test_scale_factor_out_of_range_eta_from_tbl(obj):
+    assert obj.scale_factor(pt = 50.2, eta = -2.2) == 0.8
 
-    def test_scale_factor_out_of_range_pt(self):
-        self.assertEqual(1.1726, self.obj.scale_factor(pt = 20.1, eta = 0.2))
-
-    def test_scale_factor_out_of_range_eta_from_tbl(self):
-        self.assertEqual(0.8, self.obj.scale_factor(pt = 50.2, eta = -2.2))
-
-    def test_scale_factor_out_of_range_eta_by_option(self):
-        self.assertEqual(0.8, self.obj.scale_factor(pt = 50.2, eta = -3.1))
-        self.assertEqual(0.8, self.obj.scale_factor(pt = 50.2, eta = 3.1))
+def test_scale_factor_out_of_range_eta_by_option(obj):
+    assert obj.scale_factor(pt = 50.2, eta = -3.1) == 0.8
+    assert obj.scale_factor(pt = 50.2, eta = 3.1) == 0.8
 
 ##__________________________________________________________________||
