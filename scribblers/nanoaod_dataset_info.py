c0a1e4d161bcf34d48768e07eacb6d143f2338ad
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-02-13 13:36:56 +0000

    Fix to nanoaod dataset info

--- scribblers/nanoaod_dataset_info.py
+++ scribblers/nanoaod_dataset_info.py
@@ -7,13 +7,15 @@ class DatasetInfo(object):
         self.era = [ ]
         self.nevents = [ ]
         self.nfiles = [ ]
+        self.cross_section = [ ]
         self._attach_to_event(event)
 
         self.isdata[:] = [self.component.eventtype == "Data"]
-        self.name = [self.component.dataset]
-        self.era = [self.component.era]
-        self.nevents = [self.component.nevents]
-        self.nfiles = [self.component.nfiles]
+        self.name[:] = self.component.dataset
+        self.era[:] = self.component.era
+        self.nevents[:] = [self.component.nevents]
+        self.nfiles[:] = [self.component.nfiles]
+        self.cross_section[:] = [self.component.cross_section]
 
     def _attach_to_event(self, event):
         event.isdata = self.isdata
@@ -21,6 +23,7 @@ class DatasetInfo(object):
         event.comp_era = self.era
         event.comp_nevents = self.nevents
         event.comp_nfiles = self.nfiles
+        event.cross_section = self.cross_section
 
     def event(self, event):
         self._attach_to_event(event)
